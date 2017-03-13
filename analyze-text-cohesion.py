a886b20185a754caf23f904fb707022007de7aa3
Christian Burkhart <burkhart.christian@gmail.com>
2017-03-13 17:09:58 +0100

    feat: Init knew function to analyze text cohesion

--- analyze-text-cohesion.py
+++ analyze-text-cohesion.py
@@ -1,11 +1,137 @@
 # encoding: utf-8
 
-def analyzeTextCohesion(text):
-	"""Analyzed the cohesion of a txt.
-	Args:
-		text (String) - A string that is Analyzed
-	Returns:
-		Array - An array of word pairs
-	"""
+import re
+import constants
+import subprocess
+from pygermanet import load_germanet
+from nltk.corpus import stopwords # We might need to remove this line
 
-	
+def analyzeTextCohesion(text):
+    """Analyzed the cohesion of a txt.
+    Args:
+        text (String) - A string that is Analyzed
+    Returns:
+        Array - An array of word pairs
+    """
+
+    # Check if text is string or unicode
+    if type(text) is not str:
+        raise TypeError('you did not pass a string as argument')
+
+    # Remove brackets and parenthesis from text
+    text = re.sub(r"[\(\[].*?[\)\]]", "", text)
+
+    # If text doesn't end with a dot, fill it in
+    if not text[-1:] in ['.', '!', '?']:
+            text += '.'
+
+    ############################################################################
+    # Tag text
+    ############################################################################
+    # Save text to file
+    f = open(constants.temp_text, 'w')
+    f.write(text)
+    f.close()
+
+    # Tokenize
+    f = open(constants.temp_tokens, 'w')
+    subprocess.call([constants.tokenizer, constants.temp_text], \
+        stdout=f, shell=False)
+    f.close()
+
+    # Tag Tokens from temp_tokens
+    f = open(constants.temp_tags, 'w')
+    subprocess.call([constants.rftagger, constants.german_par, \
+        constants.temp_tokens], stdout=f, shell=False)
+    f.close()
+
+    # Read tags from file
+    f = open(constants.temp_tags, 'r')
+    tags = f.readlines()
+    f.close()
+
+    # Split tags in array
+    tags = [str.split(tag, '\t') for tag in tags]
+
+    # Remove last entry
+    # the entry is only a \n character and can
+    # be ignored. It is a percularity of the
+    # RFTagger
+    tags.pop()
+
+    # Remove \n from end of tag
+    tags = [[tag[0], tag[1][:-1]] for tag in tags]
+
+    ############################################################################
+    # Further processing
+    ############################################################################
+
+    # Load germanet
+    gn = load_germanet()
+
+    # Lemmatise all words
+    tags = [{'orth': tag[0], 'lemma': gn.lemmatise(tag[0])[0],
+               'pos': tag[1]} for tag in tags]
+
+    # Filter only relevant tags: Verbs, Nouns, Pronouns
+    regex = re.compile(
+        r'.*N.Name.*|.*N.Reg.*|.*SYM.Pun.Sent.*|.*VFIN.*|.*PRO.Pers.*|.*PRO.Dem')
+
+    # Filtered tags
+    tags = [tag for tag in tags if regex.match(tag['pos']) != None]
+
+    # Is Singular?
+    tags = [dict(tag.items() + {'singular': bool(re.match(r'.*Sg',
+        tag['pos']))}.items()) for tag in tags]
+
+    # Is Feminin?
+    tags = [dict(tag.items() + {'feminin': bool(re.match(r'.*Fem',
+        tag['pos']))}.items()) for tag in tags]
+
+    # Is it a noun?
+    tags = [dict(tag.items() + {'noun': bool(re.match(r'.*N.Name.*|.*N.Reg',
+        tag['pos']))}.items()) for tag in tags]
+
+    # Is it a pronoun?
+    tags = [dict(tag.items() + {'pronoun': bool(re.match(r'.*PRO.Pers.*|.*PRO.Dem',
+        tag['pos']))}.items()) for tag in tags]
+
+    # Is it a verb?
+    tags = [dict(tag.items() + {'verb': bool(re.match(r'.*VFIN',
+        tag['pos']))}.items()) for tag in tags]
+
+
+    # Add hyponyms to nouns
+    print([tag['lemma'] for tag in tags if tag['noun'] == True])
+
+    return None
+
+
+
+text = """Im Folgenden möchte ich euch das Modell
+    der Cognitive Load Theory erklären. Diese Theorie beschreibt die beim Lernen
+    auftretenden Belastungen, bedingt durch die geringe Speicherkapazität
+    des Arbeitsgedächtnisses. Laut der Cognitive Load Theory gibt es drei
+    verschiedene Formen der Belastung: Die extrinsische Belastung,
+    die Intrinsische-Belastung und die lernbezogene Belastung. Hierbei
+    beschreibt die Extrinsische-Belastung äußere Einflüsse, die dem
+    effizienten Lernen entgegenwirken. Eine große Rolle spielt die Ablenkung.
+    Diese wird zum Beispiel gefördert, wenn ein Text nicht klar strukturiert
+    ist, da der Lernende seinen Fokus zuerst auf das ordnen des Textes legen
+    muss, bevor er den Inhalt verarbeiten kann. Ein weiteres Beispiel wäre
+    eine zu detaillierte Erklärung für einen Lernenden mit hohem Vorwissen.
+    In diesem Fall würde der Lernende von einer Informationsflut dessen,
+    was er bereits gelernt hat, abgelenkt werden. Die Intrinsische-Belastung
+    in der Cognitive-Load-Theory beschreibt die Belastung, die auf eine zu
+    komplexe Art des Lernens zurückzuführen ist. Hier kann man beispielsweise
+    das Verständnislernen anbringen: diese Art des Lernens ist wesentlich
+    effizienter als das reine Auswendiglernen von Informationen, bedeutet
+    jedoch gleichzeitig eine höhere Belastung, da man die neuen Informationen
+    mit bereits Gelerntem in Bezug setzen muss. Dies führt zu einer
+    Integration des neuen Wissens und somit zu einer Verknüpfung und
+    besserem Verständnis kommt. Die Lernbezogene-Belastung tritt bei einer
+    Kombination von hoher intrinsischer und niedriger Extrinsische-Belastung
+    zustande.  Demnach beschreibt die Cognitive-Load-Theory die Belastung,
+    der das menschliche Gehirn, besonders das Arbeitsgedächtnis, ausgesetzt."""
+
+print(analyzeTextCohesion(text))
