36c2aba4cb5762d7b9b62c009d118ebd7cbec17f
Christian Burkhart <burkhart.christian@gmail.com>
2017-03-29 12:09:00 +0200

    style: comment code and indent properly

--- analyze-text-cohesion.py
+++ analyze-text-cohesion.py
@@ -575,25 +575,17 @@ def analyzeTextCohesion(text):
                 'target': {'word': '', 'lemma': '', sentence: ''},
                 'device': 'single word'})
 
-            # wordPairs.append([word['lemma'], word['lemma'], 'lexical overlap'])
         # If there are multiple nouns append all combinations of nouns
         elif len(nouns) > 1:
-            # print(nouns)
+            # Loop over every combination of nouns in current sentence
             for subset in itertools.combinations_with_replacement(nouns_full, 2):
                 if subset[0] != subset[1]:
-                    # print(subset)
+                    # Append word pairs
                     wordPairs.append({'source': {'word': subset[0]['orth'],
-                    'lemma': subset[0]['lemma'], 'sentence': val},
-                    'target': {'word': subset[1]['orth'],
-                    'lemma': subset[1]['orth'], 'sentence': val},
-                    'device': 'within sentence'})
-                    # print(subset)
-                    # pairArray = list(subset)
-                    # # print(pairArray)
-                    # # for word in pairArray:
-                    # #     print(word)
-                    # pairArray.append('lexical overlap')
-                    # wordPairs.append(pairArray)
+                        'lemma': subset[0]['lemma'], 'sentence': val},
+                        'target': {'word': subset[1]['orth'],
+                        'lemma': subset[1]['orth'], 'sentence': val},
+                        'device': 'within sentence'})
 
     # Get hypernym hyponym pairs
     hyponym_hyper_pairs = getHypoHyperPairs(sentences, gn)
