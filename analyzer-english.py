6f823379391afc3bdcac2c33866be6b86c9039b3
Christian Burkhart <burkhart.christian@gmail.com>
2017-07-25 21:48:22 +0200

    feat: generate word pairs from nouns with spacy

--- analyzer-english.py
+++ analyzer-english.py
@@ -13,110 +13,102 @@ import spacy
 class CohesionAnalyzerEnglish:
 
     def __init__(self, text):
+        # Load spacy
+        self.nlp = spacy.load('en')
+
         # Prepare text and remove unwanted characters
-        self.text = self._prepareText(text)
+        self.text = self.nlp(text)
 
-        # Get sentences
-        self.sentences = sent_tokenize(self.text)
+        # Extract sentences
+        self.sents = self.text.sents
 
-        # Get parts of speech of text
-        self.pos = self._pos_sentences()
-
-        # Generate nouns per sentence
-        self.nouns = self._generate_nouns()
-
-        # Generate word pairs
-        self.word_pairs = self.generate_word_pairs(self.pos)
-
-
-    def _prepareText(self, text):
-        """Prepares text for further
-        data analysis. Preparation involves removal
-        of unwanted characters"""
-
-        paragraph_split = text.split('[LINEBREAK')
-
-        # Remove brackets and parenthesis from text
-        new_text = re.sub(r'[\(\[].*?[\)\]]', '', text)
-
-        # Remove percent sign
-        new_text = re.sub(r'%', '', new_text)
-
-        # Remove trailing white space
-        new_text = new_text.strip()
-
-        # Remove line breaks
-        new_text = new_text.rstrip()
-
-        # If text doesn't end with a dot, fill it in
-        if not new_text[-1:] in ['.', '!', '?']:
-                new_text += '.'
-
-        return new_text
-
-
-    def _pos_sentences(self):
-        """Generate list of sentences and their
-        pos"""
-
-        sentences = []
-
-        # Init lemmatizer
-        wordnet_lemmatizer = WordNetLemmatizer()
-
-        # Loop over every sentence with pos
-        for sentence in self.sentences:
-            # Get parts of speech per sentence
-            pos = nltk.pos_tag(nltk.word_tokenize(sentence))
-
-            # Build lemmas
-            lemma_words = [(word[0], wordnet_lemmatizer.lemmatize(word[0].lower()), word[1]) for word in pos]
-
-            # Remove punctuation from data
-            lemma_words = filter(lambda x: not bool(re.search('[,\.!?]', x[0])), lemma_words)
-
-            sentences.append(lemma_words)
-
-
-        return sentences
+        # Init empty word pairs
+        self.word_pairs = []
 
 
     def _generate_nouns(self):
         """Filter all nouns from sentences and
         return list of sentences with nouns"""
 
-        nouns = []
+        for sentence in self.sents:
 
-        for sentence in self.pos:
-            current_sentence = filter(lambda x: x[2] == 'NNP', sentence)
-            nouns.append(current_sentence)
+            # Get root from sentence
+            root = [w for w in sentence if w.head is w][0]
 
-        return nouns
+            # Get subject
+            subject = list(root.lefts)[0]
+
+            # Extract nouns from sentence
+            nouns = [word for word in sentence if any(word.pos_ in s for s in ["PROPN", "NOUN"])]
+
+            # Subject is a noun
+            if any(subject.pos_ in s for s in ["NOUN", "PROPN"]):
+                # Build word pairs
+                for noun in nouns:
+                    # Subject should not be the noun
+                    if noun.lemma_ != subject.lemma_:
+                        # Append word pair
+                        self.word_pairs.append({'source': subject.lemma_, 'target': noun.lemma_})
+            # There is no subject in the sentence
+            else:
+                # Generate all combinations
+                combs = combinations(nouns, 2)
+
+                # Loop over every combination
+                for comb in combs:
+                    self.word_pairs.append({'source': comb[0].lemma_, 'target': comb[1].lemma_})
 
 
-    def generate_word_pairs(self, sentences):
+            print self.word_pairs
 
-        word_pairs = []
+
+
+    def generate_word_pairs(self):
+
+        # word_pairs = []
+
+        self._generate_nouns()
 
         # Get word pairs from nouns
-        for sentence in self.nouns:
-            pairs_current_sentence = list(combinations(sentence, 2))
-            # print(pairs_current_sentence)
-            for pair in pairs_current_sentence:
-                word_pairs.append({'source': pair[0][1], 'target': pair[1][1]})
+        # for sentence in self.text.sents:
+            # for index, word in enumerate(sentence):
+            #     if index < len(sentence) - 1:
+            #         print '%s and %s: %f' % (word, sentence[index + 1], word.similarity(sentence[index + 1]))
 
-        return word_pairs
+        # for index, sentence in enumerate(self.text.sents):
+        #     if index < len(self.text.sents) - 1:
+        #         print '%s and %s: %f' % (sentence, self.text.sents[index + 1], sentence.similarity(self.text.sents[index + 1]))
+
+        # print '%s and %s: %f' % (self.text.sents[0], self.text.sents[1], sentence.similarity(self.text.sents[1]))
+            # print sentence
+                    # print(word.similarity(sentence[index + 1]))
+                    # print(sentence[index + 1])
+                # for w in word.subtree:
+                    # print w
+                    # print "\n"
+                # print '%s, %s, %s: %s' % (word.dep_, word.pos_, word.tag_, word.lemma_)
+
+
+            # print(sentence.lemma_)
+            # for ent in sentence.:
+            #     print(ent.label_, ent.text)
+            # print(sentence)
+            # pairs_current_sentence = list(combinations(sentence, 2))
+            # # print(pairs_current_sentence)
+            # for pair in pairs_current_sentence:
+            #     word_pairs.append({'source': pair[0][1], 'target': pair[1][1]})
+
+        # return word_pairs
 
 
 
-model = CohesionAnalyzerEnglish("""
-    Steven Arthur Pinker is a Canadian-born American cognitive scientist,
-    psychologist, linguist, and popular science author! He is
-    Johnstone Family Professor in the Department of Psychology at
-    Harvard University, and is known for his advocacy of
-    evolutionary psychology and the computational theories of mind.""")
+model = CohesionAnalyzerEnglish(u"""
+    ohn Grisham graduated from Mississippi State University before attending the University of Mississippi School of Law in 1981. He practiced criminal law for about a decade and served in the House of Representatives in Mississippi from January 1984 to September 1990.[4]
+His first novel, A Time to Kill, was published in June 1989, four years after he began writing it. As of 2012, his books have sold over 275 million copies worldwide.[5] A Galaxy British Book Awards winner, Grisham is one of only three authors to sell 2 million copies on a first printing.""")
 
-model.nouns
+# model2 = CohesionAnalyzerEnglish(u'Credit and mortgage account holders must submit their requests within 30 days')
+
+print(model.generate_word_pairs())
 
 # from nltk.stem import WordNetLemmatizer
 # wordnet_lemmatizer = WordNetLemmatizer()
