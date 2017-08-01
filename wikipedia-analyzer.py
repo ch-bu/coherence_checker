3283792d00f64e593b701756c820504076a5e705
Christian Burkhart <burkhart.christian@gmail.com>
2017-08-01 14:03:02 +0200

    chore: add script to analyze wikipedia files

---
+++ wikipedia-analyzer.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+from pandas import DataFrame
+from analyzer import analyzeTextCohesion
+import re
+
+# Path for data file
+path = '/home/christian/Repositories/goldstandardstudy-ss17/data/wikipedia_raw.csv'
+# path = '/home/christian/Downloads/test.csv'
+
+# Read data into dataframe
+data = DataFrame.from_csv(path, sep=',', index_col=False)
+        # encoding='utf-8')
+
+# Create empty data frame
+data_with_values = DataFrame(columns=('title', 'url', 'summary', 'datetime',
+    'num_sentences', 'num_clusters', 'local_cohesion',
+    'num_concepts','num_coh_sentences', 'num_non_coh_sentences',
+    'num_relations'))
+
+    # 'num_compounds_pre', 'num_coreferences_pre', 'num_stem_relations_pre',
+    # 'num_hyper_hypo_pre'))
+
+for index, row in data.iterrows():
+
+    print('%s' % (row['title']))
+
+    # Analyze current text
+    res = analyzeTextCohesion(row['summary'])
+
+    # Retrieve important variables from data
+    num_sentences = res['numSentences']
+    num_clusters = res['numCluster']
+    local_cohesion = res['local cohesion']
+    num_concepts = res['numConcepts']
+    num_coh_sentences = res['cohSentences']
+    num_non_coh_sentences = res['cohNotSentences']
+    num_relations = res['numRelations']
+
+    # Add row to data frame
+    data_with_values.loc[index] = [row['title'], row['url'], row['summary'],
+        row['datetime'],
+        num_sentences, num_clusters, local_cohesion, num_concepts,
+        num_coh_sentences, num_non_coh_sentences, num_relations]
+
+# Save data as csv
+data_with_values.to_csv(
+    '/home/christian/Repositories/goldstandardstudy-ss17/data/wikipedia-analyzed.py',
+    encoding='utf-8', index=False)
