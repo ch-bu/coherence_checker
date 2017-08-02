0e2c87e076f12341c52ef729ab2b40a776f809f4
Christian Burkhart <burkhart.christian@gmail.com>
2017-08-02 11:49:03 +0200

    feat: update script for goldstandardstudy

--- calc_simple_text_data.py
+++ calc_simple_text_data.py
@@ -5,28 +5,28 @@ from analyzer import analyzeTextCohesion
 import re
 
 # Path for data file
-path = '/home/christian/Repositories/goldstandardstudy-ss17/data/raw-data.csv'
+path = '/home/christian/Repositories/goldstandardstudy-ss17/data/study3/study3_raw_data.csv'
 
 # Read data into dataframe
-data = DataFrame.from_csv(path, sep=',', index_col=False)
+data = DataFrame.from_csv(path, sep=';', index_col=False)
         # encoding='utf-8')
 
 # Create empty data frame
-data_with_values = DataFrame(columns=('id', 'subject','lexical.overlaps'))
+data_with_values = DataFrame(columns=('id', 'age','sex',
+    'understandability_pre', 'understandability_post',
+    'pre_text', 'pre_num_sentences', 'pre_num_cluster', 'pre_local_cohesion',
+    'pre_num_concepts', 'pre_num_coh_sentences', 'pre_num_non_coh_sentences',
+    'pre_num_relations',
+    'post_text', 'post_num_sentences', 'post_num_cluster', 'post_local_cohesion',
+    'post_num_concepts', 'post_num_coh_sentences', 'post_num_non_coh_sentences',
+    'post_num_relations'))
 
     # 'num_compounds_pre', 'num_coreferences_pre', 'num_stem_relations_pre',
     # 'num_hyper_hypo_pre'))
 
 for index, row in data.iterrows():
-
-    # print(row)
-
-    # print('User %s with measure %i' % (row['subject'], row['measure']))
-
-    # print(row['pre_text'].replace('[BREAK]', ' '))
-
     # Analyze current text
-    res = analyzeTextCohesion(row['text'].replace('[BREAK]', ' '))
+    res = analyzeTextCohesion(row['pre_text'].replace('[BREAK]', ' '))
 
     # Retrieve important variables from data
     sentences_pre = res['numSentences']
@@ -36,14 +36,34 @@ for index, row in data.iterrows():
     num_coh_sentences_pre = res['cohSentences']
     num_non_coh_sentences_pre = res['cohNotSentences']
     num_relations_pre = res['numRelations']
-    num_pure_lexical_overlaps = res['numPureLexicalOverlaps']
+    # num_pure_lexical_overlaps = res['numPureLexicalOverlaps']
+
+    res_post = analyzeTextCohesion(row['post_text'].replace('[BREAK]', ' '))
+
+    # Retrieve important variables from data
+    sentences_post = res['numSentences']
+    num_clusters_post = res['numCluster']
+    local_cohesion_post = res['local cohesion']
+    num_concepts_post = res['numConcepts']
+    num_coh_sentences_post = res['cohSentences']
+    num_non_coh_sentences_post = res['cohNotSentences']
+    num_relations_post = res['numRelations']
+    # num_pure_lexical_overlaps = res['numPureLexicalOverlaps']
+
 
     # Add row to data frame
-    data_with_values.loc[index] = [row['id'], row['subject'], num_pure_lexical_overlaps]
+    data_with_values.loc[index] = [row['id'], row['age'], row['sex'],
+        row['understandability_pre'], row['understandability_post'],
+        row['pre_text'], sentences_pre, num_clusters_pre, local_cohesion_pre,
+        num_concepts_pre, num_coh_sentences_pre, num_non_coh_sentences_pre,
+        num_relations_pre,
+        row['post_text'], sentences_post, num_clusters_post, local_cohesion_post,
+        num_concepts_post, num_coh_sentences_post, num_non_coh_sentences_post,
+        num_relations_post]
 
 # Save data as csv
 data_with_values.to_csv(
-    '/home/christian/Repositories/goldstandardstudy-ss17/data/pure_lexical_overlaps.csv',
+    '/home/christian/Repositories/goldstandardstudy-ss17/data/study3/study3_data_analyzed_version1.csv',
     encoding='utf-8', index=False)
 
 # data_with_values.to_csv(
